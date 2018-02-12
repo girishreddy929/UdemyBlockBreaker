8a87ff8c6039280c506dd96ffdd15e0c1ed63c5b
giri929 <reddygirish929@gmail.com>
2018-02-12 23:34:12 +0530

    adding music

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name){
+		Debug.Log("Load Level requested for : " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit Requested ");
+		Application.Quit ();
+	}
+}
