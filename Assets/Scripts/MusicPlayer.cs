c0db6695f5c7f130d94a0b62555ed1d2d5313b1c
giri929 <reddygirish929@gmail.com>
2018-02-13 23:18:28 +0530

    editing music player

--- Assets/Scripts/MusicPlayer.cs
+++ Assets/Scripts/MusicPlayer.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MusicPlayer : MonoBehaviour {
+	static MusicPlayer instance = null;
 
 	// Use this for initialization
 	void Start () {
-		GameObject.DontDestroyOnLoad (gameObject);
+		if (instance != null) {
+			Destroy (gameObject);
+		} else {
+			instance = this;
+			GameObject.DontDestroyOnLoad (gameObject);
+		}
 	}
 	
 	// Update is called once per frame
