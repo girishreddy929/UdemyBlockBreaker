7170d81cf4a160a061cf0cbad59f31fadee288b1
giri929 <reddygirish929@gmail.com>
2018-03-01 23:57:05 +0530

    Adding lose collider

---
+++ Assets/Scripts/LooseCollider.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LooseCollider : MonoBehaviour {
+
+	public LevelManager levelmanager;
+	void OnTriggerEnter2D(Collider2D trigger){
+		print ("Trigger");
+		levelmanager.LoadLevel("Win");
+	}
+
+	void OnCollisionEnter2D(Collision2D collision){
+		print ("Collison");
+	}
+}
