f96fa2229d551f68be819ff77e0d591b0b4ab514
giri929 <reddygirish929@gmail.com>
2018-03-06 00:22:21 +0530

    Adding intial Brick Script

---
+++ Assets/Scripts/Brick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+
+	public int Maxhits;
+	private int TimesHit;
+	// Use this for initialization
+	void Start () {
+		TimesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter2D(Collision2D col){
+		TimesHit++;
+		print (TimesHit);
+	}
+}
--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -15,7 +15,7 @@ public class Paddle : MonoBehaviour {
 		Vector3 Paddlepos = new Vector3(0.5f,this.transform.position.y,0f);
 		float mouseinput = Input.mousePosition.x/Screen.width *16 ;
 
-		Paddlepos.x = Mathf.Clamp(mouseinput,0.5f,15.5f);
+		Paddlepos.x = Mathf.Clamp(mouseinput,0.75f,15.25f);
 		this.transform.position = Paddlepos;
 	}
 }
